c7c9b0f5855cfff2071b0d7858c0f8786ba0917b
shrinkunjpatel1010 <spatel@sofvare.com>
2023-11-24 20:01:19 +0530

    - passmana logo change

--- lib/utility/assets_utility/assets_paths.dart
+++ lib/utility/assets_utility/assets_paths.dart
@@ -4,8 +4,8 @@
 
 class AppAssets {
   //pass mana logo without text asset path
-  static const String appLogoWithoutText = 'assets/pass_mana_app_logo_image.png';
+  static const String appLogoWithoutText = 'assets/pass_mana_app_logo_image.jpg';
 
   //pass mana logo with text asset path
-  static const String appLogoWithText = 'assets/pass_mana_app_logo_with_name.png';
+  static const String appLogoWithText = 'assets/pass_mana_app_logo_with_name.jpg';
 }
