e97e00e6254bbcdcc299109f4abf98a02919ecf4
shrinkunjpatel1010 <spatel@sofvare.com>
2023-11-30 16:13:46 +0530

    - navigation bar route list ,icon list,text list moved to constants

--- lib/presentation/navigation_bar_screen.dart
+++ lib/presentation/navigation_bar_screen.dart
@@ -3,11 +3,8 @@
 */
 
 import 'package:flutter/material.dart';
-import 'package:passmana/localization/app_localization.dart';
 import 'package:passmana/presentation/common/custom_bottom_navigation_bar.dart';
-import 'package:passmana/utility/color.dart';
-import 'package:passmana/utility/page_routes_utility/page_routes.dart';
-import 'package:passmana/utility/text_utility/text_styles.dart';
+import 'package:passmana/utility/constants.dart';
 import 'package:passmana/utility/utility.dart';
 
 class NavigationBarScreen extends StatefulWidget {
@@ -23,112 +20,6 @@ class NavigationBarScreen extends StatefulWidget {
 }
 
 class _NavigationBarScreenState extends State<NavigationBarScreen> {
-  List<Widget> notSelectedIconWidgets = const <Widget>[
-    Icon(
-      Icons.lock_outline_rounded,
-      color: AppColors.mWhite,
-      size: 24,
-    ),
-    Icon(
-      Icons.people_outline,
-      color: AppColors.mWhite,
-      size: 30,
-    ),
-    Icon(
-      Icons.add,
-      color: AppColors.mWhite,
-      size: 30,
-    ),
-    Icon(
-      Icons.alt_route,
-      color: AppColors.mWhite,
-      size: 22,
-    ),
-    Icon(
-      Icons.settings_outlined,
-      color: AppColors.mWhite,
-      size: 25,
-    ),
-  ];
-  List<Widget> notSelectedTextWidgets = <Widget>[
-    Text(
-      getTranslated('password'),
-      style: TextStyles.getTitleWhiteText(10),
-    ),
-    Text(
-      getTranslated('group'),
-      style: TextStyles.getTitleWhiteText(10),
-    ),
-    Text(
-      getTranslated('create'),
-      style: TextStyles.getTitleWhiteText(10),
-    ),
-    Text(
-      getTranslated('generate'),
-      style: TextStyles.getTitleWhiteText(10),
-    ),
-    Text(
-      getTranslated('settings'),
-      style: TextStyles.getTitleWhiteText(10),
-    ),
-  ];
-  List<Widget> selectedIconWidgets = <Widget>[
-    Icon(
-      Icons.lock,
-      color: AppColors.secondaryMaterialColor[400],
-      size: 24,
-    ),
-    Icon(
-      Icons.people,
-      color: AppColors.secondaryMaterialColor[400],
-      size: 30,
-    ),
-    const Icon(
-      Icons.add,
-      color: AppColors.mWhite,
-      size: 30,
-    ),
-    Icon(
-      Icons.alt_route,
-      color: AppColors.secondaryMaterialColor[400],
-      size: 22,
-    ),
-    Icon(
-      Icons.settings,
-      color: AppColors.secondaryMaterialColor[400],
-      size: 25,
-    )
-  ];
-  List<Widget> selectedTextWidgets = <Widget>[
-    Text(
-      getTranslated('password'),
-      style: TextStyles.getTitleDarkRedText(10),
-    ),
-    Text(
-      getTranslated('group'),
-      style: TextStyles.getTitleDarkRedText(10),
-    ),
-    Text(
-      getTranslated('create'),
-      style: TextStyles.getTitleDarkRedText(10),
-    ),
-    Text(
-      getTranslated('generate'),
-      style: TextStyles.getTitleDarkRedText(10),
-    ),
-    Text(
-      getTranslated('settings'),
-      style: TextStyles.getTitleDarkRedText(10),
-    ),
-  ];
-  List<String> routeList = [
-    AppRoutes.passwordList,
-    AppRoutes.groupList,
-    AppRoutes.createList,
-    AppRoutes.generatePassword,
-    AppRoutes.settings,
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -140,11 +31,11 @@ class _NavigationBarScreenState extends State<NavigationBarScreen> {
             right: 10,
             left: 10,
             child: CustomBottomNavigationBar(
-              routeList: routeList,
-              notSelectedIconWidgets: notSelectedIconWidgets,
-              notSelectedTextWidgets: notSelectedTextWidgets,
-              selectedIconWidgets: selectedIconWidgets,
-              selectedTextWidgets: selectedTextWidgets,
+              routeList: AppConstants.bottomNavigationRouteList,
+              notSelectedIconWidgets: AppConstants.bottomNavigationNotSelectedIconWidgets,
+              notSelectedTextWidgets: AppConstants.bottomNavigationNotSelectedTextWidgets,
+              selectedIconWidgets: AppConstants.bottomNavigationSelectedIconWidgets,
+              selectedTextWidgets: AppConstants.bottomNavigationSelectedTextWidgets,
               selectedIndex: Utility.getCurrentIndex(),
             ),
           ),
--- lib/utility/constants.dart
+++ lib/utility/constants.dart
@@ -2,6 +2,12 @@
 * Created by Shrikunj Patel on 9/13/2023.
 */
 
+import 'package:flutter/material.dart';
+import 'package:passmana/localization/app_localization.dart';
+import 'package:passmana/utility/color.dart';
+import 'package:passmana/utility/page_routes_utility/page_routes.dart';
+import 'package:passmana/utility/text_utility/text_styles.dart';
+
 class AppConstants {
   //language constants
   static const String english = 'en';
@@ -10,4 +16,112 @@ class AppConstants {
 
   //hero animation tags
   static const String appLogo = "appLogo";
+
+
+  //navigation bar screen constants
+  static List<Widget> bottomNavigationNotSelectedIconWidgets = const <Widget>[
+    Icon(
+      Icons.lock_outline_rounded,
+      color: AppColors.mWhite,
+      size: 24,
+    ),
+    Icon(
+      Icons.people_outline,
+      color: AppColors.mWhite,
+      size: 30,
+    ),
+    Icon(
+      Icons.add,
+      color: AppColors.mWhite,
+      size: 30,
+    ),
+    Icon(
+      Icons.alt_route,
+      color: AppColors.mWhite,
+      size: 22,
+    ),
+    Icon(
+      Icons.settings_outlined,
+      color: AppColors.mWhite,
+      size: 25,
+    ),
+  ];
+  static List<Widget> bottomNavigationNotSelectedTextWidgets = <Widget>[
+    Text(
+      getTranslated('password'),
+      style: TextStyles.getTitleWhiteText(10),
+    ),
+    Text(
+      getTranslated('group'),
+      style: TextStyles.getTitleWhiteText(10),
+    ),
+    Text(
+      getTranslated('create'),
+      style: TextStyles.getTitleWhiteText(10),
+    ),
+    Text(
+      getTranslated('generate'),
+      style: TextStyles.getTitleWhiteText(10),
+    ),
+    Text(
+      getTranslated('settings'),
+      style: TextStyles.getTitleWhiteText(10),
+    ),
+  ];
+  static List<Widget> bottomNavigationSelectedIconWidgets = <Widget>[
+    Icon(
+      Icons.lock,
+      color: AppColors.secondaryMaterialColor[400],
+      size: 24,
+    ),
+    Icon(
+      Icons.people,
+      color: AppColors.secondaryMaterialColor[400],
+      size: 30,
+    ),
+    const Icon(
+      Icons.add,
+      color: AppColors.mWhite,
+      size: 30,
+    ),
+    Icon(
+      Icons.alt_route,
+      color: AppColors.secondaryMaterialColor[400],
+      size: 22,
+    ),
+    Icon(
+      Icons.settings,
+      color: AppColors.secondaryMaterialColor[400],
+      size: 25,
+    )
+  ];
+  static List<Widget> bottomNavigationSelectedTextWidgets = <Widget>[
+    Text(
+      getTranslated('password'),
+      style: TextStyles.getTitleDarkRedText(10),
+    ),
+    Text(
+      getTranslated('group'),
+      style: TextStyles.getTitleDarkRedText(10),
+    ),
+    Text(
+      getTranslated('create'),
+      style: TextStyles.getTitleDarkRedText(10),
+    ),
+    Text(
+      getTranslated('generate'),
+      style: TextStyles.getTitleDarkRedText(10),
+    ),
+    Text(
+      getTranslated('settings'),
+      style: TextStyles.getTitleDarkRedText(10),
+    ),
+  ];
+  static List<String> bottomNavigationRouteList = [
+    AppRoutes.passwordList,
+    AppRoutes.groupList,
+    AppRoutes.createList,
+    AppRoutes.generatePassword,
+    AppRoutes.settings,
+  ];
 }
