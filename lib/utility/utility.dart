56ac72907cb0c6fbf2bff66fd256097b757d9a6f
shrinkunjpatel1010 <138668207+shrinkunjpatel1010@users.noreply.github.com>
2024-06-05 15:28:37 +0530

    - added some words for add/edit card screen

--- lib/utility/utility.dart
+++ lib/utility/utility.dart
@@ -43,8 +43,8 @@ class Utility {
         end: Alignment.bottomRight,
         stops: const [0.10, 0.90],
         colors: [
-          AppColors.accentPrimaryColor.withOpacity(0.7),
-          AppColors.accentSecondaryColor.withOpacity(0.7),
+          AppColors.accentPrimaryColor.withOpacity(0.9),
+          AppColors.accentSecondaryColor.withOpacity(0.9),
         ],
       ),
     );
